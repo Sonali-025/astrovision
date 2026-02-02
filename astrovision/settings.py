1c86c641dbe84f75f00ece429a6f45e549fe49ca
Sonali Priya <sonali025@gmail.com>
2026-02-02 21:43:58 +0530

    Switch email backend to SendGrid

--- astrovision/settings.py
+++ astrovision/settings.py
@@ -144,9 +144,9 @@ if EMAIL_ENABLED:
     EMAIL_HOST = "smtp.sendgrid.net"
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_PASSWORD")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 
-    EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
     DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
